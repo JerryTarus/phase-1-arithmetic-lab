f05d1a0370087917136880acd0ed0ff67717f518
Jerry Kipchirchir <jerrytarus@gmail.com>
2023-09-25 13:37:09 +0300

    Completed Arithmetic Operators Lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+// Multiplying two Numbers
+let num1 = 62;
+let num2 = 1; 
+let multiply = num1 * num2;
+
+
+// Generating random number between 1 and 1000
+let random = Math.floor(Math.random() * 1000) + 1;
+
+// Implementing the Modulus Operator
+let num3 = 401;
+let num4 = 39;
+let mod = num3 % num4;
+
+// Picking a maximum number from a range
+let max = Math.max(2, 7, 9, 12, 14, 15, 20);
+
+console.log(max);
